2d114bbf74ee2da54007421d281d58555246d705
Vitor Aoki <vitor-27@hotmail.com>
2018-09-18 23:27:05 -0300

    Teste

--- Tutorial Django/mysite/polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# . significa este diretorio (polls -> polls.models)
-from .models import Question, Choice
-
-admin.site.register(Question)
-admin.site.register(Choice)
