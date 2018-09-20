430b33cb2fa57b34251eef5f85812147c0cfc2c6
Vitor Aoki <vitor-27@hotmail.com>
2018-09-20 01:11:12 -0300

    Método de status do boleto adicionado

--- Módulo/pagamento/servico/urls.py
+++ Módulo/pagamento/servico/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^compra_com_cartao/$', views.CompraComCartao, name='Compra Cartao'),
+    url(r'^input=(?P<pk>\d+)', views.input,  name='input'),
+    url(r'^status_boleto', views.status_boleto, name='status_boleto'),
 ]
