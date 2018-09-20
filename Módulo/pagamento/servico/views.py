75a2faec12629754fdc4a4adb66ec8252a80336c
HeitorBoschirolli <heitor.boschirolli@gmail.com>
2018-09-20 01:06:07 -0300

    mudancas para evitar conflito feitas

--- Módulo/pagamento/servico/views.py
+++ Módulo/pagamento/servico/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -9,15 +11,68 @@ from .utils import *
 def index(request):
     return HttpResponse("Hello, world. You're at the payment index.")
 
+def CompraComCartao(request):
+    if request.method == 'POST':
+        form_cartao = PagamentoCartaoForm(data=request.POST)
+        context = {
+            'form_cartao': form_cartao,
+            'status': 1,
+            'mensagem': 'Pagamento Aprovado',
+            'pk_pagamento': 1,
+        }
+        return render(request, 'pagamento_cartao.html', context)
+
+    else:
+        form_cartao = PagamentoCartaoForm()
+        return render(request, 'pagamento_cartao.html', {'form_cartao': form_cartao})
+
+
+    #return render(request, 'pagamento_cartao_interface.html', context)
+    # mensagem = ''
+
+    # # Cartao
+    # num_cartao = request.POST('num_cartao', '123')
+    # cvv = request.POST('cvv', '123')
+    # nome_cartao = request.POST('nome_cartao', 'Paulo')
+    # data_vencimento_cartao_str = request.POST('data_vencimento_cartao', '11-2018')
+    # credito = request.POST('credito', '1')
+    # num_parcelas = int(request.POST('num_parcelas', '2'))
+    # pedido = request.POST('pedido', '1')
+
+    # # Pedido
+    # cpf_cliente = request.POST('cpf_cliente', '123')
+    # cnpj_empresa = request.POST('cnpj_empresa', '123')
+    # valor = int(request.POST('valor', '123'))
+    # data_emissao = request.POST('data_emissao', '10-12-2018')
+
+
+    # ### Início Tratamento de erros ###
+
+    # # Se cartão estiver vencido retorna erro
+    # hoje = datetime.now()
+    # data_vencimento_cartao = datetime.strptime('data_vencimento_cartao_str', '%m-%Y')
+    # if not hoje < data_vencimento_cartao:
+    #     mensagem = 'Cartão Vencido'
+
+    # # Se for débito com parcelas retorna erro
+    # if num_parcelas > 1 and credito == 0:
+    #     mensagem = 'Numero de parcelas não condiz com o tipo de cartão'
+
+    # if valor <= 0:
+    #     mensagem = 'Valor incorreto'
+
+    # ### Fim Tratamento de erros ###
+
+
 def pagamento_boleto(request):
-    form_cartao = PagamentoForm()
+    form_cartao = PagamentoBoletoForm()
     context = {
         'form_cartao': form_cartao,
     }
     return render(request, 'servico/get_infos_boleto.html', context)
 
 def status_boleto(request):
-    form_boleto = PagamentoForm(data=request.POST)
+    form_boleto = PagamentoBoletoForm(data=request.POST)
 
     cpf_comprador = form_boleto['cpf_comprador'].value()
     valor_compra = form_boleto['valor_compra'].value()
