9470558487c890e23cccf5ebfb04443815e80889
Vitor Aoki <vitor-27@hotmail.com>
2018-10-21 12:15:39 -0300

    views arrumada

--- modulo/pagamento/backend/views.py
+++ modulo/pagamento/backend/views.py
@@ -15,7 +15,7 @@ url_clientes = "ec2-18-231-28-232.sa-east-1.compute.amazonaws.com:3002/"
 
 #Renderiza a pagina inicial do site
 def home(request):
-    _ = get_produtos(request)
+    # _ = get_produtos(request)
 
     return render(request, 'backend/home.html')
 
@@ -422,6 +422,118 @@ def att_produto(request):
 
         return JsonResponse({'error': e})
 
+'''---------------------------------------------------------------------------------------------------------'''
+'''--------------------------------------------API DE PAGAMENTO---------------------------------------------'''
+'''---------------------------------------------------------------------------------------------------------'''
+
+
+#Realiza um pagamento por cartão
+def pagamento_cartao(request):
+
+    #Recupera os dados do pagamento para enviar para a api de pagamento
+    # forms_pagamento = Pagamento(data=request.POST)
+
+    #URL para api de pagamento
+    url = 'http://pagamento.4pmv2bgufu.sa-east-1.elasticbeanstalk.com/servico/pagamento_cartao'
+
+    # Variaveis de teste
+    data = {
+        "cpf_comprador": "12356712345",
+        "valor_compra": "10.20",
+        "cnpj_site": "12345678992735",
+        "data_emissao_pedido": "2/10/2018",
+        "numero_cartao": "1234123412341111",
+        "nome_cartao": "SINDAO",
+        "cvv_cartao": "123",
+        "data_vencimento_cartao": "2/10/2025",
+        "credito": "1",
+        "num_parcelas": "2"
+    }
+
+    data = json.dumps(data)
+
+    request2 = urllib2.Request(url=url, data=data, headers={'Content-Type': 'application/json'})
+
+    try:
+
+        serializade_data = urllib2.urlopen(request2).read()
+        resposta = json.loads(serializade_data)
+
+
+        return JsonResponse(resposta)
+
+    except Exception as e:
+        return JsonResponse({'error': e.code})
+
+
+#Realiza um pagamento por boleto
+def pagamento_boleto(request):
+
+    #Recupera os dados do pagamento para enviar para a api de pagamento
+    # forms_pagamento = Pagamento(data=request.POST)
+
+    #URL para api de pagamento
+    url = 'http://pagamento.4pmv2bgufu.sa-east-1.elasticbeanstalk.com/servico/pagamento_boleto'
+
+    # Variaveis de teste
+    data = {
+        "cpf_comprador": "12356712345",
+        "valor_compra":"10.20",
+        "cnpj_site":"12345678992735",
+        "banco_gerador_boleto":"Itau",
+        "data_vencimento_boleto":"04/10/2018",
+        "endereco_fisico_site":"Rua Sindo",
+        "data_emissao_pedido": "25/06/2018"
+    }
+
+    data = json.dumps(data)
+
+    request2 = urllib2.Request(url=url, data=data, headers={'Content-Type': 'application/json'})
+
+    try:
+
+        serializade_data = urllib2.urlopen(request2).read()
+        resposta = json.loads(serializade_data)
+
+
+        return JsonResponse(resposta)
+
+    except Exception as e:
+
+        return JsonResponse({'error': e.code})
+
+#Consulta um pagamento
+def consulta_pagamento(request):
+
+    #Recupera os dados do pagamento para enviar para a api de pagamento
+    # forms_pagamento = Pagamento(data=request.POST)
+
+    #URL para api de pagamento
+    url = 'http://pagamento.4pmv2bgufu.sa-east-1.elasticbeanstalk.com/servico/busca_pedido'
+
+    # Variaveis de teste
+    data = {
+        "pk_pagamento": "1"
+    }
+
+    data = json.dumps(data)
+
+    request2 = urllib2.Request(url=url, data=data, headers={'Content-Type': 'application/json'})
+
+    try:
+
+        serializade_data = urllib2.urlopen(request2).read()
+        resposta = json.loads(serializade_data)
+
+
+        return JsonResponse(resposta)
+
+    except Exception as e:
+
+        return JsonResponse({'error': e.code})
+
+def pagamento(request):
+    return render(request=request, template_name='backend/pagamento.html')
 
 def meu_carrinho(request):
     # Pega o usuário caso exista
@@ -450,5 +562,3 @@ def meu_carrinho(request):
 
         return JsonResponse({'error': e.code})
 
-def pagamento(request):
-    return render(request=request, template_name='backend/pagamento.html')
